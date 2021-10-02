2cfe92de31846cb2717fb43f4c0afacbc7e5a40f
LENIN309 <yairca734@gmail.com>
2021-10-02 00:59:19 +0000

    excuse

--- src/app.js
+++ src/app.js
@@ -8,4 +8,28 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const type1 = function() {
+    const getRandomInt = Math.floor(Math.random() * who.length);
+    const randomWho = who[getRandomInt];
+    // const randomAction = action[getRandomInt];
+    // const randomWhat = who[getRandomInt];
+    // const randomWhen = who[getRandomInt];
+    console.log(randomWho);
+    return `${randomWho}`;
+  };
+  type1();
+
+  document.getElementById("excuse").innerHTML = type1;
 };
